07b059c479f5eb3d0ea2c30735070556026eafe1
Freeezy <62744775+FilippoPiumatti@users.noreply.github.com>
2022-02-05 13:04:00 +0100

    TEORIA_PHP

---
+++ Es 1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>Es1 PHP</title>
+</head>
+<body id="page-top">
+<h1>Prima pagina PHP</h1>
+<?php
+echo("Prima stringa scritta da un comando PHP");
+$num = 15;
+echo("<h2>Valore della variabile numero = $num</h2>");
+?>
+
+
+<footer class="bg-black small text-center">
+    <div class="container">
+        <h3>Copyright &copy; 4D Info 2022</h3>
+    </div>
+</footer>
+</body>
+</html>
